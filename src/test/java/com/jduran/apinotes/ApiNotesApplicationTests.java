8efb85895c053e7d182035c82a7f5638494a537f
jonathanduran7 <joniduran624@gmail.com>
2024-04-14 18:43:58 -0300

    first commit

---
+++ src/test/java/com/jduran/apinotes/ApiNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jduran.apinotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
